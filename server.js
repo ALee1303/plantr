06fc1e44e5d4b741c86fe1da4c0ccd7940e35349
Tori Pope <toricpope@gmail.com>
2019-01-22 19:36:08 -0600

    web page setup

--- server.js
+++ server.js
@@ -18,6 +18,22 @@ server.get('/', async (req, res, next) => {
   }
 });
 
+server.get('/gardeners', (req, res, next) => {
+  res.redirect('/');
+});
+
+server.get('/gardeners/:id', async (req, res, next) => {
+  try {
+    let id = req.params.id;
+    const gardener = await Gardener.findById(id, {
+      include: [{ all: true, nested: true }],
+    });
+    res.send(gardener);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`Server listening on port ${3000}`);
